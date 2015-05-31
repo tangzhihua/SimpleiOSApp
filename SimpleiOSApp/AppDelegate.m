66c3c157e4db4b5624122af5e6b68b669c77dfaf
tangzhihua <3252475@qq.com>
2015-05-31 18:18:28 +0800

    111

--- SimpleiOSApp/AppDelegate.m
+++ SimpleiOSApp/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
   [self.window makeKeyAndVisible];
- //test
+ //test 
   return YES;
 }
 
