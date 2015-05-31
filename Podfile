7c526dcaf75cf58c95d5640a958f7269359b364e
tangzhihua <3252475@qq.com>
2015-05-31 17:58:19 +0800

    add first

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '6.0'
+
+pod 'RNTimer', '~> 0.0.1'
+pod 'WEPopover', '~> 1.0.0'
+pod 'NSString-UrlEncode', '~> 1.1.0'
+pod 'JSONKit', '~> 1.5pre'
+pod 'GDChannel', '~> 0.5'
---
+++ SimpleiOSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleiOSApp
+//
+//  Created by skyduck on 15/5/31.
+//  Copyright (c) 2015年 Skyduck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
