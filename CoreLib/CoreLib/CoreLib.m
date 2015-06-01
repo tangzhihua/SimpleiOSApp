0ce39126903eeef73676f3338a055d3ef2d65fc6
tangzhihua <3252475@qq.com>
2015-06-01 14:05:10 +0800

    创建项目基础框架

---
+++ CoreLib/CoreLib/CoreLib.m
@@ -0,0 +1,13 @@
+//
+//  CoreLib.m
+//  CoreLib
+//
+//  Created by skyduck on 15/6/1.
+//  Copyright (c) 2015年 Skyduck. All rights reserved.
+//
+
+#import "CoreLib.h"
+
+@implementation CoreLib
+
+@end
--- SimpleiOSApp/AppDelegate.m
+++ SimpleiOSApp/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
- 
+
+#import "CoreLib.h"
 @interface AppDelegate ()
 
 @end
@@ -18,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
   [self.window makeKeyAndVisible];
+   
  //test       gggg 
   return YES;
 }
